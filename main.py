5153bd9f035b1cb20652791dbf42c84866170707
EnzoAguirre04 <ea.aguirre@alumno.um.edu.ar>
2025-08-10 15:50:17 -0300

    Arreglos finales del código inicial del juego. 6.5.0

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+Computación I - Juego de Ta-Te-Ti.
+Nombre y Apellido: Enzo Agustín Aguirre Polenta.
+Ciclo Lectivo: 2025.
+Carrera: Ingeniería en Informática.
+Ruta: "computaci-n-2025-08-05-ta-te-ti-EnzoAguirre04/main.py".
+"""
+
+## Inicio del código.
+
+# Inicio de imports.
+
+from src.cli import ejecutar_juego
+
+# Fin de imports.
+
+# Inicio de la Ejecución del juego.
+
+if __name__ == "__main__":
+    ejecutar_juego()
+
+# Fin de la Ejecución del juego.
+
+## Fin del código.
